026737d78e6b6cf14b9a76af86ebae60275f30ab
erikbettencourt <embettencourt@gmail.com>
2025-05-15 11:56:12 -0400

    Add proper component structure and default export

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
-{/* Add TrackRecommendations component to the track detail view */}
-{selectedTrack && (
-  <TrackRecommendations referenceTrack={selectedTrack} />
-)}
+import React from 'react';
+
+function App() {
+  const [selectedTrack, setSelectedTrack] = React.useState(null);
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      {/* Add TrackRecommendations component to the track detail view */}
+      {selectedTrack && (
+        <TrackRecommendations referenceTrack={selectedTrack} />
+      )}
+    </div>
+  );
+}
+
+export default App;
